1d3f355d3ab4fb6a1512c7c6f9fbe4776d7a32b3
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-04-16 13:39:02 +0200

    added user route and model

--- startup/routes.js
+++ startup/routes.js
@@ -2,11 +2,14 @@ const express = require('express');
 const customers = require('../routes/customers'); 
 const products = require('../routes/products'); 
 const sales = require('../routes/sales'); 
+const users = require ('../routes/users')
 
 
 module.exports = function(app){
     app.use(express.json());
+
 app.use('/api/customers', customers);
+app.use('/api/users', users);
 app.use('/api/products', products);
 app.use('/api/sales', sales);
     }
