85dd55514905635521f171a6cfca6e93ca245083
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-04-16 14:11:19 +0200

    added auth route

--- startup/routes.js
+++ startup/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const auth = require('../routes/auth');
 const customers = require('../routes/customers'); 
 const products = require('../routes/products'); 
 const sales = require('../routes/sales'); 
@@ -10,6 +11,7 @@ module.exports = function(app){
 
 app.use('/api/customers', customers);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 app.use('/api/products', products);
 app.use('/api/sales', sales);
     }
